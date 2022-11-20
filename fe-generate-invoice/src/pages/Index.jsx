77d3179156144c849c044f1e2826eb4f05f6152f
Ken <musyaffa560@gmail.com>
2022-11-20 19:58:26 +0700

    fixing slicing landing page without login and register page

---
+++ fe-generate-invoice/src/pages/Index.jsx
@@ -0,0 +1,56 @@
+import Header from "../component/Header"
+import { useState } from "react"
+
+import Admin from "../component/Admin";
+import Client from "../component/Client";
+
+export default function Home(){
+    const tipeUser = ["klien", "admin"];
+    const [ myShow, setShow] = useState('')
+    return (
+        <>
+            <Header name="GIS"/>
+            <div className="hero container d-flex flex-row justify-content-between">
+                <div className="hero-left">
+                    <h1 className="mb-4">Heading 1</h1>
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. (Heading 3)</h3>
+                    <div className="hero-button d-flex flex-row mt-4">
+                        <div className="border border-dark daftar"><p>Daftar</p></div>
+                        {/* <div className="border border-dark dapatkan"><p>Dapatkan Aplikasi</p></div> */}
+                    </div>
+                </div>
+                <div className="hero-right">
+                    
+                </div>
+            </div>
+            <div className="main container">
+                <h2 className="title-main text-center mb-4">Generate Invoice System</h2>
+                <h4 className="desc-title-main text-center" id="fitur">(Tentang Aplikasi)Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.</h4>
+                {/* <div className="options-tab">
+                    <div className="client-side text-center">
+                        <p>Client</p>
+                    </div>
+                    <div className="admin-side text-center">
+                        <p>Admin</p>
+                    </div>
+                </div> */}
+                <div className="options-tab">
+                {tipeUser.map(tipeUser=>(
+                    <button
+                        type="button" 
+                        key={tipeUser} 
+                        className={"client-admin text-center"}
+                        onClick={() => setShow(tipeUser)}
+                    >
+                    {tipeUser.toLocaleUpperCase()}
+                    </button>
+                ))}
+                </div>
+                <div>
+                    {myShow === "admin" && (<Admin/>)}
+                    {myShow === "klien" && (<Client/>)}
+                </div>
+            </div>
+        </>
+    )
+}
