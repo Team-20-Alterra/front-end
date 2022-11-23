250c921c7ee2fdd5c772a5b9e2808dc2247a2459
mfauzan8 <mhmmdfauzan296@gmail.com>
2022-11-23 08:56:21 +0700

    fix: modify design from uiux and install react-icons

--- fe-generate-invoice/src/pages/Login.jsx
+++ fe-generate-invoice/src/pages/Login.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import loginImage from '../assets/image/pana.png';
+import loginImage from '../assets/image/login-image.png';
+import { FcGoogle } from 'react-icons/fc'
 
 const Login = () => {
     return (
         <div className="loginWrap">
-            <div className="containerLogin">
-                <div className='loginImage'>
+            <div className="containerLogin d-flex flex-column justify-content-center">
+                <p className='logoLogin text-center'> Ginap</p>
+                <div className='loginImage d-flex justify-content-center'>
                     <img src={loginImage} alt="loginImage" />
                 </div>
                 <div className="containerInput">
                     <div className="input">
-                        <span className="icon"><i className="bi bi-person-circle"></i></span>
+                        <span className="icon"><i className="bi bi-person-fill"></i></span>
                         <input type="text" id="login" placeholder="User ID" />
                     </div>
                     <div className="input">
@@ -21,7 +23,7 @@ const Login = () => {
                 <a href='/' className='forgetPassword' >Lupa kata sandi?</a>
                 <button type="button" className="btn-primary">Masuk</button>
                 <div className="text-divider">Atau masuk dengan</div>
-                <button type="button" className="btn-secondary">Google</button>
+                <button type="button" className="btn-secondary"><FcGoogle /> Google</button>
                 <div className="containerTextRegister">
                     <a href='/register' className='textRegister' >Belum punya akun?</a>
                 </div>
