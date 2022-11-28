3e209acd71641d5fad7c396121eff71eb387c0be
Zhafran Afif <zhafranafif97@gmail.com>
2022-11-28 21:03:09 +0700

    adding riwayat list feature

--- fe-generate-invoice/src/pages/Dashboard/RiwayatPage.jsx
+++ fe-generate-invoice/src/pages/Dashboard/RiwayatPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { HiFilter } from "react-icons/hi"
 import RiwayatEmpty from '../../component/DashboardFeature/RiwayatEmpty'
 import HeaderDashboard from '../../component/DashboardFeature/HeaderDashboard'
+import ListRiwayat from '../../component/DashboardFeature/ListRiwayat'
 
 const RiwayatPage = () => {
     return (
@@ -30,8 +31,8 @@ const RiwayatPage = () => {
                 <input type="text" placeholder='Cari' />
                 <button><HiFilter className='filter-icon' />Filter</button>
             </div>
-            <div className='riwayat-page__empty'>
-                <RiwayatEmpty />
+            <div className='riwayat-list__container'>
+                <ListRiwayat/>
             </div>
         </div>
     )
