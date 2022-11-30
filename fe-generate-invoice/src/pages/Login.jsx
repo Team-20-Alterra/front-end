3740297c3e4e3c9f10d2972e16fd7c1620d0a924
Zhafran Afif <zhafranafif97@gmail.com>
2022-11-30 19:57:50 +0700

    testing login API

--- fe-generate-invoice/src/pages/Login.jsx
+++ fe-generate-invoice/src/pages/Login.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import loginImage from '../assets/image/Auth/login-image.png';
 import { FcGoogle } from 'react-icons/fc'
+import { useState } from 'react';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const navigate = useNavigate()
+    const [values, setValues] = useState({
+        email: "",
+        password: "",
+        showPassword: false
+    })
+
+    const handleLoginButton = (event) => {
+        event.preventDefault()
+        // eslint-disable-next-line no-unused-expressions
+        axios({
+            method: 'POST',
+            url: "http://ec2-18-181-241-210.ap-northeast-1.compute.amazonaws.com:8000/api/v1/login",
+            data: {
+                email: values.email,
+                password: values.password
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+        }
+        )
+        .then(response => console.log(response.data.token))
+        .catch(error => console.log(error))
+    }
+
     return (
         <div className="loginWrap">
             <div className="containerLogin d-flex flex-column justify-content-center">
@@ -10,16 +40,17 @@ const Login = () => {
                 <div className='loginImage d-flex justify-content-center'>
                     <img src={loginImage} alt="loginImage" />
                 </div>
-                <div className="containerInput">
+                <form className="containerInput" onSubmit={handleLoginButton}>
                     <div className="input">
                         <span className="icon"><i className="bi bi-person-fill"></i></span>
-                        <input type="text" id="login" placeholder="User ID" />
+                        <input type='email' id="login" placeholder="User ID" onChange={(e) => setValues({...values, email : e.target.value})}/>
                     </div>
                     <div className="input">
                         <span className="icon"><i className="bi bi-lock-fill"></i></span>
-                        <input type="text" id="login" placeholder="Kata Sandi" />
+                        <input type="password" id="login" placeholder="Kata Sandi" onChange={(e) => setValues({...values, password : e.target.value})} />
                     </div>
-                </div>
+                    <input type="submit" />
+                </form>
                 <a href='/' className='forgetPassword' >Lupa kata sandi?</a>
                 <button type="button" className="btn-primary" id='ButtonMasuk'>Masuk</button>
                 <div className="text-divider">Atau masuk dengan</div>
